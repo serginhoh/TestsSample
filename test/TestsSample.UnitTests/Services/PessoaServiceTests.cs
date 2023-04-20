94a8b4dd269c04a9643947a18a1fa22d6edd32b9
Sergio Henrique P. Oliveira <serginho@linx.com.br>
2023-04-20 12:16:01 -0300

    Moq + Teste Erro

--- test/TestsSample.UnitTests/Services/PessoaServiceTests.cs
+++ test/TestsSample.UnitTests/Services/PessoaServiceTests.cs
@@ -1,4 +1,5 @@
-﻿using FluentValidation;
+﻿using Castle.Core.Resource;
+using FluentValidation;
 using Moq;
 using TestsSample.Interfaces;
 using TestsSample.Models;
@@ -22,5 +23,39 @@ public class PessoaServiceTests
         Assert.NotNull(result);
         Assert.Equal(pessoa, result);
         Assert.IsAssignableFrom<Pessoa>(result);
+    }
+
+    [Fact()]
+    public async Task AdicionaPessoa_Deve_Retornar_Pessoa_Moq_Validator()
+    {
+        //Arrange
+        Mock<IValidator<Pessoa>> validator = new();
+        IPessoaService service = new PessoaService(validator.Object);
+        Pessoa pessoa = new() { Nome = "Nome Teste", Aniversario = new DateTime(2000, 1, 1) };
+
+        //Act
+        var result = await service.AdicionaPessoa(pessoa);
+
+        //Assert
+        Assert.NotNull(result);
+        Assert.Equal(pessoa, result);
+        Assert.IsAssignableFrom<Pessoa>(result);
+    }
+
+    [Fact()]
+    public async Task AdicionaPessoa_Deve_Retornar_Erro()
+    {
+        //Arrange
+        InlineValidator<Pessoa> validator = new ();
+        validator.RuleFor(i => i.Nome).Must(i => false);
+        IPessoaService service = new PessoaService(validator);
+        Pessoa pessoa = new() {  };
+
+        //Act
+        var result = await Assert.ThrowsAsync<ValidationException>(() => service.AdicionaPessoa(pessoa));
+
+        //Assert
+        Assert.NotNull(result);
+        Assert.IsAssignableFrom<ValidationException>(result);
     }
 }
